83980c582636e7b8ad6f7abc954215634dff8483
Kapilan Srikaran <kapilansrikaran@gmail.com>
2021-04-27 16:19:40 +0530

    Add files via upload

---
+++ C Programing Essentials/structure.c
@@ -0,0 +1,33 @@
+/*
+* File Name: structure.c
+* Author: Kapilan Srikaran
+* date: 27/04/2021
+* description: Structure of a C Program
+*/
+
+#include <stdio.h> //preprocessor
+
+int a=5; //global Variable
+
+int main(){
+	//Every C programming must have one main function in a file 
+	//Body of main function
+	
+	int b=5; //local variable
+	
+	printf("Welcome to the C programming");	
+	
+	return 0;
+}
+
+// NOTE:
+// We can use void infront of the function name; 
+// If you use the void not want to put return 0;  the last line 
+
+/*
+void car(){
+
+	printf("Welcome to the C programming");
+	
+}
+*/
