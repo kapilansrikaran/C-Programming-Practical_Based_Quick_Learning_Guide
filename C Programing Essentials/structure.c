5b22b7f5f6d90a6f85ed0f478eac9914678d9e14
Kapilan Srikaran <kapilansrikaran@gmail.com>
2021-04-27 16:21:32 +0530

    Update structure.c

--- C Programing Essentials/structure.c
+++ C Programing Essentials/structure.c
@@ -8,22 +8,18 @@
 #include <stdio.h> //preprocessor
 
 int a=5; //global Variable
-
 int main(){
 	//Every C programming must have one main function in a file 
 	//Body of main function
-	
 	int b=5; //local variable
-	
 	printf("Welcome to the C programming");	
-	
 	return 0;
 }
+
 
 // NOTE:
 // We can use void infront of the function name; 
 // If you use the void not want to put return 0;  the last line 
-
 /*
 void car(){
 
