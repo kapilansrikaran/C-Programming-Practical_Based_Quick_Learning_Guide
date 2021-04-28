e1d167e5327f1608967dd4dfae3513448b8c78b5
Kapilan Srikaran <kapilansrikaran@gmail.com>
2021-04-28 11:00:17 +0530

    Add files via upload

---
+++ C Programing Essentials/printBio.c
@@ -0,0 +1,18 @@
+/*
+* File Name: printBio.c
+* Author: Kapilan Srikaran
+* date: 28/04/2021
+* description: Using to printf and print personal Details 
+*/
+
+#include <stdio.h>
+
+int main(){
+	printf("Personal Details\n");
+	printf("-----------------\n");
+	printf("Name:    Kapilan Srikaran\n");
+	printf("Country: Sri Lanka\n");
+
+	return 0;
+}
+
