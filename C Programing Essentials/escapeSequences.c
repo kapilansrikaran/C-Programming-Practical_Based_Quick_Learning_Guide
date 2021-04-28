fcb16ed6f3f374d1bb5e53f832cb05ae311507a3
Kapilan Srikaran <kapilansrikaran@gmail.com>
2021-04-28 11:29:03 +0530

    Add files via upload

---
+++ C Programing Essentials/escapeSequences.c
@@ -0,0 +1,37 @@
+/*
+* File Name: escapeSequences.c
+* Author: Kapilan Srikaran
+* date: 28/04/2021
+* description: use escape sequences to print special characters like new line, tab space, quotes etc. within printf( ) statements 
+*/
+
+#include <stdio.h>
+
+int main(){
+	printf("Hello\nWorld");			//use of \n
+	printf("\nHello\tWorld");		//use of \t
+	printf("\n\"Hello World!\"");	//use of \"
+    printf("\nHello\bWorld!");		//use of \b backspace use to delete the one character
+    
+    printf("\n\n");
+    //Bill printing
+    printf("Item Name\tPrice");
+    printf("\n---------\t-----");
+    printf("\nBanana\t\tRs:150");
+    printf("\nApple\t\tRs:200");
+    printf("\nOrange\t\tRs:40");
+    
+	return 0;
+}
+
+
+/*
+Commonly used escape sequences are:
+ \n (newline)
+ \t (tab)
+ \v (vertical tab)
+ \b (backspace)
+ \r (carriage return)
+ \” (double quotes)
+ \’ (single quotes)
+*/
