049d7897366b2c112200c96762cc90b668ffd497
Kapilan Srikaran <kapilansrikaran@gmail.com>
2021-12-18 17:35:40 +0530

    Update 3-escapeSequences.c

--- C Programing Essentials/3-escapeSequences.c
+++ C Programing Essentials/3-escapeSequences.c
@@ -10,16 +10,16 @@
 int main(){
 	printf("Hello\nWorld");			//use of \n
 	printf("\nHello\tWorld");		//use of \t
-	printf("\n\"Hello World!\"");	//use of \"
-    printf("\nHello\bWorld!");		//use of \b backspace use to delete the one character
+	printf("\n\"Hello World!\"");		//use of \"
+    	printf("\nHello\bWorld!");		//use of \b backspace use to delete the one character
     
-    printf("\n\n");
-    //Bill printing
-    printf("Item Name\tPrice");
-    printf("\n---------\t-----");
-    printf("\nBanana\t\tRs:150");
-    printf("\nApple\t\tRs:200");
-    printf("\nOrange\t\tRs:40");
+    	printf("\n\n");
+    	//Bill printing
+    	printf("Item Name\tPrice");
+    	printf("\n---------\t-----");
+    	printf("\nBanana\t\tRs:150");
+    	printf("\nApple\t\tRs:200");
+    	printf("\nOrange\t\tRs:40");
     
 	return 0;
 }
